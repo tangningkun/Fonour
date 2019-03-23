f38c91cc74dc09dbe784217cd7355febddd2f66e
TangNingKun <1209229446@qq.com>
2019-03-23 22:21:30 +0800

    权限

---
+++ 依赖注入/TsBlog/src/Libraries/TsBlog.Domain/Entities/Role.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TsBlog.Domain.Entities
+{
+    public class Role:Entity
+    {
+        /// <summary>
+        /// 角色ID
+        /// </summary>
+        public Guid RoleId { get; set;}
+        /// <summary>
+        /// 角色名称
+        /// </summary>
+        public string RoleName { get; set; }
+        /// <summary>
+        /// 角色类型
+        /// </summary>
+        public int RoleType { get; set; }
+
+        /// <summary>
+        /// 角色编码
+        /// </summary>
+        public string RoleCode { get; set; }
+        /// <summary>
+        /// 创建人Id
+        /// </summary>
+        public Guid CreateUserId { get; set; }
+        /// <summary>
+        /// 角色备注
+        /// </summary>
+        public string Remarks { get; set; }
+    }
+}
